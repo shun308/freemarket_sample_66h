b02acc13bcdfa66371b09c30918b50f5dc32b0d1
Souichirou Takeuchi <pnskh112pnskh112pnskh112@gmail.com>
2020-01-13 14:58:53 +0900

    commit_add_basic_auth

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -2,5 +2,5 @@ class Shipping < ApplicationRecord
   has_one :product
   validates :fee, presence: true
   validates :region, presence: true
-  validates :period_before_shipping: true
+  validates :period_before_shipping, presence: true
 end
