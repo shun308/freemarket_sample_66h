d56dbf7f25aa99297a585a32c03937d0d897c5f5
yuuta31 <odk.hello@gmail.com>
2020-01-13 21:45:09 +0900

    conding_user_scope_and_address_and_products
    #what
    
    as a title

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   
   devise_for :users
+
+  devise_scope :user do
+    # ログイン・ログアウト
+    get    'login',                 to: 'users/sessions#new'
+    post   'login',                 to: 'users/sessions#create'
+    delete 'logout',                to: 'users/sessions#destroy'
+    # 新規登録
+    get    'signup',                to: 'users/registrations#index'
+    get    'signup/registration',   to: 'users/registrations#new'
+  end
+
   root 'roots#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :address except: :show
+
+  resources :products
+
 end
