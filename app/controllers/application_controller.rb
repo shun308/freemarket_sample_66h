923ab7fb7d875a5448aaf577b9a7f98bcd321255
Souichirou Takeuchi <pnskh112pnskh112pnskh112@gmail.com>
2020-01-13 10:41:37 +0900

    commit_to_basic_auth
    ベーシック認証のため変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
-  # def hello
-  #   render html:  "
-  #                   aaaaaaaa
-  #                   takeutiです
-  #                   kamataです
-  #                   oginoです、からの
-  #                   髙橋です。
-  #                   hello, world!
-  #                 "
-  # end
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
