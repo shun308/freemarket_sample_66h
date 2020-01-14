f64c807677bfa7e9873bf9022e77fff30a3aac5d
Souichirou Takeuchi <pnskh112pnskh112pnskh112@gmail.com>
2020-01-14 12:40:43 +0900

    commit_to_deploy_auto

--- Gemfile
+++ Gemfile
@@ -86,3 +86,5 @@ gem 'omniauth-facebook'
 gem 'omniauth-google-oauth2'
 gem 'dotenv-rails'
 gem 'omniauth-rails_csrf_protection'
+
+gem 'fog-aws'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,4 @@
 
-server '13.112.247.135', user: 'ec2-user', roles: %w{app db web}
-
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -10,6 +8,7 @@ server '13.112.247.135', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '13.112.247.135', user: 'ec2-user', roles: %w{app db web}
 
 
 # role-based syntax
