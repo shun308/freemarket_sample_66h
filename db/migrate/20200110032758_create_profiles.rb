903d110dce988e9ff0c7d6ab465797da1613a166
Souichirou Takeuchi <pnskh112pnskh112pnskh112@gmail.com>
2020-01-14 21:55:19 +0900

    commit_to_upd_migrate
    マイグレーションファイルの更新
    （
    　profilesテーブルに顧客情報（住所など）追加
    　addressテーブルに送り先氏名追加
    ）

--- db/migrate/20200110032758_create_profiles.rb
+++ db/migrate/20200110032758_create_profiles.rb
@@ -10,7 +10,14 @@ class CreateProfiles < ActiveRecord::Migration[5.2]
       t.integer     :birth_year, null: false
       t.integer     :birth_month, null: false
       t.integer     :birth_day, null: false
-      t.integer     :phone_number, unique: true
+      # t.integer     :phone_number, unique: true
+
+      t.string      :living_code
+      t.integer     :living_state
+      t.string      :living_city
+      t.string      :living_number
+      t.string      :living_building
+
       t.references  :user,null: false, foreign_key: true
       t.timestamps
     end
