f469516e0e8413ede9bada70e7f020c76712178d
Souichirou Takeuchi <pnskh112pnskh112pnskh112@gmail.com>
2020-01-15 17:33:11 +0900

    to_add_address_function
    アドレス登録画面の実装
    実際にユーザと紐づくアドレスが登録される処理を実装

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -9,9 +9,8 @@ class AddressController < ApplicationController
 
 
   def create
-    binding.pry
     Adresse.create(adresse_params)
-    # redirect_to
+    redirect_to dones_path
   end
 
   def edit
@@ -24,7 +23,7 @@ class AddressController < ApplicationController
   
   private
   def adresse_params
-    params.require(:adresse).permit(:zipcode, :prefecture, :city, :district, :building)
+    params.require(:adresse).permit(:zipcode, :prefecture, :city, :district, :building,:first_name,:last_name,:first_name_kana,:last_name_kana,:phone_number).merge(user_id: current_user.id)
   end
 
 end
