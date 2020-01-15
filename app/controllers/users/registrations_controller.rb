18f6231c601fc5e008b313ceb548fc6dceb041dd
yuuta31 <odk.hello@gmail.com>
2020-01-15 15:46:53 +0900

    fix_error_routes
    #what
    
    as a title

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -57,7 +57,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    address_new_path
+    new_address_path
   end
 
   # The path used after sign up for inactive accounts.
