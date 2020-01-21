6434ff94d4e8b31cd1b70cddd507e6fedea405cd
shun308 <takahashi0903shun@gmail.com>
2020-01-21 13:54:37 +0900

    add table

---
+++ db/migrate/20200121041038_create_products.rb
@@ -0,0 +1,21 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.integer :size, null: false, default: 0
+      t.text :description, null: false
+      t.integer :brand,null: false, default: 0
+      t.integer :price, null: false, index: true
+      t.integer :shipping_method, null: false, default: 0
+      t.integer :region, null: false, default: 0
+      t.integer :period_before_shipping, null: false, default: 0
+      t.integer :fee, null: false, index: true, default: 0
+      t.integer :sell_state, null: false, default: 0
+      t.integer :condition, null: false, default: 0
+      t.string :name, null: false, index: true
+      t.integer :category_id, null: false, default: 0
+      #t.references :category, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
