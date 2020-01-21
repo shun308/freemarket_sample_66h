2b7096d606fb44c7616cf3081b16ce22f274dbb7
shun308 <takahashi0903shun@gmail.com>
2020-01-21 17:36:01 +0900

    add enum

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,13 @@ class Product < ApplicationRecord
   #has_one :order
   #has_many :likes
 
+  enum category_id: {
+    レディース:1,メンズ:2,ベビー・キッズ:3,インテリア・住まい・小物:4,本・音楽・ゲーム:5,おもちゃ・ホビー・グッズ:6,コスメ・香水・美容:7,
+    家電・スマホ・カメラ:8,スポーツ・レジャー:9,ハンドメイド:10,チケット:11,自転車・オートバイ:12,その他:13
+    },_suffix: true
+
   enum fee: {
-    "---":0,送料込み（出品者負担）:1,着払い（購入者負担）:2
+    送料込み（出品者負担）:1,着払い（購入者負担）:2
     }
 
   # enum shipping_method: {
@@ -15,7 +20,6 @@ class Product < ApplicationRecord
   # }
 
   enum region:{
-    "---":0,
     北海道:1,青森県:2,岩手県:3,宮城県:4,秋田県:5,山形県:6,福島県:7,
     茨城県:8,栃木県:9,群馬県:10,埼玉県:11,千葉県:12,東京都:13,神奈川県:14,
     新潟県:15,富山県:16,石川県:17,福井県:18,山梨県:19,長野県:20,
@@ -27,7 +31,6 @@ class Product < ApplicationRecord
     },_suffix: true
 
   enum period_before_shipping:{
-    "---":0,
     "1~2日で発送":1, "2~3日で発送":2, "4~7日で発送":3
   },_suffix: true
 
