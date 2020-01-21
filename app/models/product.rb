137ce86fcce890b557cc2cf31e9bdd5018672444
shun308 <takahashi0903shun@gmail.com>
2020-01-21 14:05:27 +0900

    add model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  has_many :images, dependent: :delete_all
+  #has_many :comments
+  belongs_to :category
+  belongs_to :user
+  #has_one :order
+  #has_many :likes
 end
