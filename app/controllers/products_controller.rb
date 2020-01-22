fea029153b667026ceb57052efc9c6a9dc6b8886
shun308 <takahashi0903shun@gmail.com>
2020-01-22 18:13:51 +0900

    delete @product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to products_path(@product)
+      redirect_to products_path
     else
       render 'products/new'
     end
