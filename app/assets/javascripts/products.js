553f7c1ea5e46f09b691ffc58f07d0f0cb77698e
shun308 <takahashi0903shun@gmail.com>
2020-01-23 23:37:08 +0900

    change product.js

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,15 +3,15 @@ $(document).on('turbolinks:load', ()=> {
   const buildFileField = (num)=> {
     const html = `<div data-index="${num}" class="js-file_group">
                     <input class="js-file" type="file"
-                    name="product[images_attributes][${num}][src]"
-                    id="product_images_attributes_${num}_src"><br>
+                    name="product[images_attributes][${num}][image]"
+                    id="product_images_attributes_${num}_image"><br>
                     <div class="js-remove">削除</div>
                   </div>`;
     return html;
   }
   // プレビュー用のimgタグを生成する関数
   const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    const html = `<img data-index="${index}" image="${url}" width="100px" height="100px">`;
     return html;
   }
 
