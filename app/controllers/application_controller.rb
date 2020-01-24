d48030bbad5216e4cd88eb567a27c48bc1281087
oginotsutomu <oginotsutomugigi@icloud.com>
2020-01-24 21:24:52 +0900

    修正
    コメントアウト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-  # before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   private
@@ -14,9 +13,5 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  # end
 
 end
--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -18,12 +18,10 @@ class PurchaseController < ApplicationController
     end
     @products = Product.find(params[:id])
     @products_image = Product.order('created_at DESC').limit(1)
-    # @product = Product.with_attached_photos.find(params[:id])
     @products_price = (@products.price.to_i).round
   end
 
   def pay
-    # @parents = Category.where(ancestry:nil)
     @products = Product.find(params[:id])
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = Rails.application.credentials.PAYJP_PRIVATE_KEY
@@ -32,29 +30,19 @@ class PurchaseController < ApplicationController
     customer: card.customer_id, #顧客ID
     currency: 'jpy', #日本円
     )
-    # redirect_to root_path 
     redirect_to action: 'done', id:Product.find(params[:id])
-    # 上のredirect_toは、削除予定
-    # redirect_to action: 'done', id:Product.find(params[:id]) #完了画面に移動
   end
 
   def done
-    # @parents = Category.where(ancestry:nil)
     card = Card.where(user_id: current_user.id).first
     customer = Payjp::Customer.retrieve(card.customer_id)
     @default_card_information = customer.cards.retrieve(card.card_id)
     @card_brand = card_brand
     @products = Product.find(params[:id])
     @products_image = Product.order('created_at DESC').limit(1)
-    # @product = Product.with_attached_photos.find(params[:id])
     @products_price = (@products.price.to_i).round
     @products.sell_state = 2
     @products.save
-
-    # @product_purchaser= Product.find(params[:id])#購入者のidをpurchaser_idに保存
-    # @product_purchaser.update( purchaser_id: current_user.id)
-    # @product_update_condition = find_by(:sell_state)
-    # @product_update_condition.update
   end
 
   def card_brand
